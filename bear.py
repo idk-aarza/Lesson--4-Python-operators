d1fd0d14450722961648d0a16177b497f2ca267f
idk-aarza <aarzak2012@gmail.com>
2024-10-14 18:46:52 +0530

    bear

---
+++ bear.py
@@ -0,0 +1,15 @@
+gift1=20
+gift2=45
+gift3=50
+gift4=15
+print(gift1 + gift2 + gift3 + gift4)
+print(gift3 - gift4)
+print(gift1/gift4)
+print(gift2 // gift4)
+print(gift3 * gift2 )
+print(50 % 15 )
+print(gift1 ** gift4)
+no= int(input("Enter your no."))
+no2=int(input('Enter your second no.'))
+percentage= ((no + no2 )/200)*100
+print(percentage)
